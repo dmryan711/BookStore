6f7f13d67319afa8c136db09f84b3dca1f78b66c
Devon <dmryan711@gmail.com>
2019-06-10 19:08:07 -0400

    Added all field values for mongoDB in books document

--- models/Books.js
+++ models/Books.js
@@ -6,6 +6,24 @@ const BooksSchema = new Schema({
     title: {
         type:String,
         required:[true,"A title is required"]
+    },
+    description:{
+        type:String,
+        required:[true, "A description is required"]
+    },
+    authors:[{
+        type:String,
+        required:[true, "Authors are required"]
+
+    }],
+    image:{
+        type: String,
+        data:Buffer,
+        require:[true, "An image is required"]
+    },
+    link:{
+        type:String,
+        require:[true, "A link is required"]
     }
 });
 
