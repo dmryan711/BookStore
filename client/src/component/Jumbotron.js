86388d5d834cb7fe1c4abac2e513d806862a0d5e
Devon <dmryan711@gmail.com>
2019-06-11 13:39:37 -0400

    Added Navbar and jumbotron

---
+++ client/src/component/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Jumbotron() {
+  return (
+    <div class="jumbotron">
+        <h1 class="display-4">React Google Book Search</h1>
+        <p class="lead">Search for and Save Books of Interest</p>
+  </div>
+  
+
+  );
+}
+
+export default Jumbotron;
