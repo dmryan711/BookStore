e1c83ff405f7f950c03314cba965ede50663b1ee
Devon <dmryan711@gmail.com>
2019-06-18 20:10:01 -0400

    Minor bug to resolve warning

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 function Navbar() {
   return (
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand">Google Books</a>
+  <div className="navbar-brand">Google Books</div>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
